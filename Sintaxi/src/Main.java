efdfa36dc16210ee8302895205b94a0c090abb14
Alberto Luna <123046909@UPQ.EDU.MX>
2025-04-03 23:44:04 -0600

    .

---
+++ Sintaxi/src/Main.java
@@ -0,0 +1,46 @@
+//importacion
+//clase principal
+/*
+soy un
+comentariooo
+
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        /*
+holaaaaaaaaaa
+         */
+        System.out.println("Hello,Alberto Luna");
+        String cadenas = "alberto " + " luna " + " rufino";
+
+        System.out.println(cadenas);
+        System.out.println(cadenas.length());
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0,5));
+
+
+        int x=5,x2;
+        double y=1.25,y2;
+        String z = "grupos204",z1;
+
+        x2=Integer.parseInt(z);
+        z1=String.valueOf(Y);
+        System.out.println(X2);
+        System.out.println(z1);
+
+        double asd = Double.valueOf(x);
+
+        int num=12;
+        double numD=num;
+
+        System.out.println("comversiones"+numD);
+
+        System.out.println(x2);
+        System.out.println(z1);
+        System.out.println(asd);
+
+
+    }
+}
